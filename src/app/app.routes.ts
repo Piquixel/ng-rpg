3a82375767541158dc2087c23f8491ab75c380c1
wsomdev <56587373+wsomdev@users.noreply.github.com>
2026-03-24 20:29:55 +0100

    create user

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LandingPage } from './pages/landing-page/landing-page';
+import { CreateCharacterPage } from './pages/create-character-page/create-character-page';
 
 export const routes: Routes = [
   { path: 'landing', component: LandingPage },
+  { path: 'create-character', component: CreateCharacterPage },
   { path: '**', redirectTo: 'landing' },
 ];
