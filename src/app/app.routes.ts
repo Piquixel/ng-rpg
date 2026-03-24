71b2d2586f5ccae1c41ef0d0ad61ae2970586d96
wsomdev <56587373+wsomdev@users.noreply.github.com>
2026-03-24 20:02:09 +0100

    Create landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LandingPage } from './pages/landing-page/landing-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'landing', component: LandingPage },
+  { path: '**', redirectTo: 'landing' },
+];
