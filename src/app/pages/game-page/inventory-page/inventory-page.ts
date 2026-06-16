2f144116cfbfea6d5c1434c29b111a2026dcac07
Piquixel <thomas.fetu2004@gmail.com>
2026-06-16 13:11:10 +0200

    update 7 files

--- src/app/pages/game-page/inventory-page/inventory-page.ts
+++ src/app/pages/game-page/inventory-page/inventory-page.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { TitleCasePipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Player } from 'interfaces/player.interface';
+import { GameManagerService } from 'models/services/game-manager.service';
+import { InterfaceDigitsPipe } from '../../../pipes/interface-digits-pipe';
 
 @Component({
   selector: 'app-inventory-page',
-  imports: [],
+  imports: [TitleCasePipe, InterfaceDigitsPipe],
   templateUrl: './inventory-page.html',
   styleUrl: './inventory-page.scss',
 })
-export class InventoryPage {}
+export class InventoryPage {
+  private _gameManager: GameManagerService = inject(GameManagerService);
+  public get playerStats(): Player {
+    return this._gameManager.currentPlayer;
+  }
+}
